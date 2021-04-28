1e7c0c2f0bc43bcab0464b0175de7602bd625434
monjasa <monja.nya@gmail.com>
2021-04-28 23:13:20 +0300

    Initial commit

---
+++ src/main/java/org/monjasa/projectclinica/ProjectClinicaApplication.java
@@ -0,0 +1,13 @@
+package org.monjasa.projectclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectClinicaApplication.class, args);
+    }
+
+}
